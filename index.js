78aa3a56f93e6f27c96328da1ee81d175e48069d
Bradford Knowlton <brad@ucanbonline.net>
2017-10-26 18:26:12 -0600

    awesome version

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var shopifyAPI = require('shopify-node-api');
 
 var request = require('request').defaults({ encoding: null });
 
+var trim = require('trim');
+var slug = require('slug');
+
 var Shopify = new shopifyAPI({
   shop: process.env.SHOP, // MYSHOP.myshopify.com  // process.env.shop
   apiKey: process.env.API_KEY, // Your API key 
@@ -34,6 +37,7 @@ console.log(process.env.ACCESS_TOKEN);
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
+  	res.writeHead(200, {'Content-Type': 'text/html'});
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path;
@@ -57,45 +61,162 @@ http.createServer(function (req, res) {
 
      var artist = workSheetsFromFile[0].data[1];
 
-     console.log(artist[3]);
-     console.log(artist[9]);
+     workSheetsFromFile[0].data.shift();
+
+     // var test = [];
+
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+     // test.push(workSheetsFromFile[0].data.pop());
+
      
-     request.get(artist[9], function (error, response, body) {
+    // console.log(test);
+		workSheetsFromFile[0].data.forEach(function(currentValue, currentIndex, listObj){
+		// test.forEach(function(currentValue, currentIndex, listObj){
+
+		// console.log(slug(listObj[currentIndex][3]).toLowerCase());
+		// res.write( '<li>' + slug(listObj[currentIndex][3].toLowerCase()) + '</li>');
+
+		var handle = '';
+
+		if( listObj[currentIndex][3] ){
+			handle = slug(listObj[currentIndex][3].toLowerCase());
+		}
+
+		var title = '';
+		if( listObj[currentIndex][3] ){
+			title = trim(listObj[currentIndex][3]);
+		}
+
+
+		var instagramURL = '';
+		var facebookURL = '';
+		var websiteURL = '';
+
+
+		if( listObj[currentIndex][6] && trim(listObj[currentIndex][6]) != '' ){
+			// console.log(instagram(listObj[currentIndex][6]));
+			// res.write( '<li>' + instagram(listObj[currentIndex][6]) + '</li>');
+			instagramURL = instagram(listObj[currentIndex][6]);
+		}else{
+			// console.log('N/A');
+			// res.write('<li>N/A</li>');
+		}
+
+		if( listObj[currentIndex][5] && trim(listObj[currentIndex][5]) != '' ){
+			// console.log(facebook(listObj[currentIndex][5]));
+			// res.write( '<li>' + facebook(listObj[currentIndex][5]) + '</li>');
+			facebookURL = facebook(listObj[currentIndex][5]);
+		}else{
+			// console.log('N/A');
+			// res.write('<li>N/A</li>');
+		}
+
+		if( listObj[currentIndex][4] && trim(listObj[currentIndex][4]) != '' ){
+			// console.log(website(listObj[currentIndex][4]));
+			// res.write( '<li>' + facebook(listObj[currentIndex][5]) + '</li>');
+			websiteURL = website(listObj[currentIndex][4]);
+		}else{
+			// console.log('N/A');
+			// res.write('<li>N/A</li>');
+		}
+
+		var body_html = '';
+		body_html = (listObj[currentIndex][8])? '<p class="p1">'+listObj[currentIndex][8].replace('\r\n','</p><p>').replace('\r\n','</p><p>').replace('\r\n','</p><p>').replace('\r\n','</p><p>').replace('\r\n','</p><p>')+'</p>':'' ;
+		body_html = body_html + ' <p class="p1">';
+		body_html = body_html + ((body_html)? ' ':'' ) + ((websiteURL != '')?''+createURL(websiteURL, 'WEBSITE'):'' );
+
+		body_html = body_html + ((websiteURL != '' && ( facebookURL != '' || instagramURL != '' ) )? ' // ':'');
+
+		body_html = body_html + ((facebookURL != '')?''+createURL(facebookURL, 'FACEBOOK'):'' );
+
+		body_html = body_html + ((facebookURL != '' && instagramURL != '' )? ' // ':'');
+
+		body_html = body_html + ((instagramURL != '')?''+createURL(instagramURL, 'INSTAGRAM'):'' );
+		body_html = body_html + ' </p>';
+
+		body_html = trim(body_html);
+
+		// console.log(body_html);
+
+		// console.log(facebookURL); 
+		var tags = [];
+
+		if( listObj[currentIndex][7] ){
+			tags = tags.concat( listObj[currentIndex][7].split(/\r?\n/) );
+		}
+
+		if( listObj[currentIndex][0].includes("BOTH") ){
+			tags.push('Missoula Holiday MADE fair');
+			tags.push('Helena Holiday MADE fair');
+		}else if( listObj[currentIndex][0].includes("Missoula") ){
+			tags.push('Missoula Holiday MADE fair');
+		}else if( listObj[currentIndex][0].includes("Helena") ){
+			tags.push('Helena Holiday MADE fair');
+		}
+		
+
+		tags = tags.join(',');
+		
+		wait(1000);
+		console.log('waiting....');
+
+		request.get(listObj[currentIndex][9], function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
 	        attachment = "" + new Buffer(body).toString('base64');
 	        // console.log(attachment);
 
 	         var post_data = {
 			  "product": {
-			    "title": artist[3],
-			    "body_html": artist[8],
+			  	"handle": handle,
+			    "title": title,
+			    "body_html": body_html,
 			    "vendor": "",
-			    "product_type": "",
+			    "product_type": "artist",
 			    "published_scope": "global",
 			    "images": [
 			      {
 			        "attachment": attachment
 			      }
 			    ],
-			    "tags": artist[7].split(/\r?\n/).join(','),
+			    "tags": tags,
 			    "variants": [
 			    ]
 			  }
 			}
 			
-			console.log(post_data); 
+			// console.log(post_data); 
 
 			Shopify.post('/admin/products.json', post_data, function(err, data, headers){
 			  console.log(err);
 			  console.log(data);
-			//  console.log(headers);
+			  console.log(headers);
+			  if(data){
+			  	res.write(JSON.stringify(data));
+			  }else if(err){
+			  	res.write(JSON.stringify(err));
+			  }
+
+			  wait(1000);
+			  console.log('waiting....');
+			  
 			});
 
+	    }else{
+	    	console.log(listObj[currentIndex][9]);
+	    	console.log('Request error: '+error);
 	    }
+
 	});
 
-    
-
+	});    
 
  });
   } else {
@@ -107,3 +228,73 @@ http.createServer(function (req, res) {
     return res.end();
   }
 }).listen(8080); 
+
+
+function instagram(url){
+	var baseUrl = 'https://www.instagram.com/';
+
+	username = url.toLowerCase().split("?")[0];
+	username = username.replace('www.instagram.com','');
+	username = username.replace('instagram.com','');
+	username = username.replace('https://www.','');
+	username = username.replace('http://www.','');
+	username = username.replace('https:','');
+	username = username.replace('http:','');
+	username = username.replace('/','').replace('/','').replace('/','').replace('/','').replace('/','');
+	username = username.replace('@','');
+	username = username.replace("'",'').replace("'",'');
+	username = username.replace(' ','').replace(' ','');
+
+	username = trim(username);
+
+	return baseUrl+username;
+}
+
+function facebook(url){
+	var baseUrl = 'https://www.facebook.com/';
+
+	username = url.toLowerCase().split("?")[0];
+	username = username.replace('www.facebook.com','');
+	username = username.replace('facebook.com','');
+	username = username.replace('https://www.','');
+	username = username.replace('http://www.','');
+	username = username.replace('https:','');
+	username = username.replace('http:','');
+	username = username.replace('/','').replace('/','').replace('/','').replace('/','').replace('/','');
+	username = username.replace('@','');
+	username = username.replace("'",'').replace("'",'');
+	username = username.replace(' ','').replace(' ','');
+
+	username = trim(username);
+
+	return baseUrl+username;
+}
+
+function website(url){
+	var baseUrl = '';
+
+
+	url = trim(url);
+
+	url = url.replace(' ','').replace(' ','').replace(' ','').replace(' ','').replace(' ','').replace(' ','').replace(' ','');
+
+	if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
+
+	return url;
+}
+
+
+function createURL(url, string){
+	return '<a href="'+url+'" target="_BLANK">'+string+'</a>';
+}
+
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
